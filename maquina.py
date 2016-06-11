3b7c543e06714b01adaa382779405d4ec4870212
Humberto Vieira <humbertovieira.castro@gmail.com>
2016-06-11 23:13:04 +0000

    funfa

--- maquina.py
+++ maquina.py
@@ -6,11 +6,11 @@ last_received = ''
 
 # Pegar o token quando o eze entra
 # token caso de merda - EAAWajIcUJAgBAKvLmCTxEEWd51gudNRwwwZC1md8nlttxrznK5knCidraWgIyibA2v3TRLSHYdJy8BKtGpuEoFZAI5E2SRTQS8C83HXZC5WhBmnBqDzZBO4HwdTzniTryvcqX47TfxxjoCbezCqloh4uRTfArNdTouNhVXv1zwZDZD
-graph = facebook.GraphAPI(access_token='EAACEdEose0cBAO7q9BKxPCBqWFrlQLZBEFr6coz0hQZCOZCLEeSo7DRX4fvIDTmmUFuvZCoofVkTgTpUjguNHTCMFOa0lF4iJjUfZChSyQzTXwS6ZAh5R5NVZAfyPQhLbBfyGeckMemUaph4XsfPOSZAiKqjNKqSThC5KTFJ79hCiwZDZD', version='2.2')
+graph = facebook.GraphAPI(access_token='EAACEdEose0cBAGXSTscSLHCwG5VFn1FS9Mp0pKjbrJEeSD49BUrrdWVfBqtxH8LgS7n1HeLoysmEOuSGF2z26hJ3vqCZAnY9I2Oun5K0rkPRkDpYQSAbcXHsK85ZAZCdqyIZCv88gWF52X5lROwPV4ZA4tu87AZA2MkZCVmGPpdtQZDZD', version='2.2')
 
 # Esse da pra postar 
 graph.put_object(parent_object='me', connection_name='feed',
-                 message='Muito legal!', place='298042960354301')
+                 message='Muito lega5luj!', place='298042960354301')
 
 # Checkin produto
 # graph.put_object(parent_object='me', connection_name='feed',
@@ -23,12 +23,18 @@ graph.put_object(parent_object='me', connection_name='feed',
 
 
 
-ser = serial.Serial(port='/dev/ttyACM1', baudrate=115200)
+ser = serial.Serial(port='/dev/ttyACM2', baudrate=115200)
 
 def recieving():
     while True:
-        print('legal')
-        print(ser.readline())
+        retorno = ser.readline()
+
+        if retorno == 'a\r\n':
+            print('entrou')
+        
+        print(retorno)
+
+        
         # ser.write(b'')
     ser.close()
 
