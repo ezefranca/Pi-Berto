07a4683137c0d2c815d08a9709c399d5ff18b8ed
Humberto Vieira <humbertovieira.castro@gmail.com>
2016-06-12 01:27:10 +0000

    rodando incrivelmente rasp quase final sem o token

--- maquina.py
+++ maquina.py
@@ -1,16 +1,15 @@
 import serial
 from threading import Thread
 import facebook
+from random import *
 
 last_received = ''
 
 # Pegar o token quando o eze entra
 # token caso de merda - EAAWajIcUJAgBAKvLmCTxEEWd51gudNRwwwZC1md8nlttxrznK5knCidraWgIyibA2v3TRLSHYdJy8BKtGpuEoFZAI5E2SRTQS8C83HXZC5WhBmnBqDzZBO4HwdTzniTryvcqX47TfxxjoCbezCqloh4uRTfArNdTouNhVXv1zwZDZD
-graph = facebook.GraphAPI(access_token='EAACEdEose0cBAGXSTscSLHCwG5VFn1FS9Mp0pKjbrJEeSD49BUrrdWVfBqtxH8LgS7n1HeLoysmEOuSGF2z26hJ3vqCZAnY9I2Oun5K0rkPRkDpYQSAbcXHsK85ZAZCdqyIZCv88gWF52X5lROwPV4ZA4tu87AZA2MkZCVmGPpdtQZDZD', version='2.2')
+graph = facebook.GraphAPI(access_token='EAACEdEose0cBAPABgJZCJ724E5HyHzZCG2LG59YKtTGcjrnrs2HOhS1dUNf5lyNeZCZC0RWxKp0YZCSDgs8d5vBzuO0iuhlalaKBw50HL7kKtrTVKjMZBwaZCE5IhUwz13XMI9VZBhq7RANo3jLgWdGdnMVOrrI9z0GKZAADZA5iGY4gZDZD', version='2.2')
 
-# Esse da pra postar 
-graph.put_object(parent_object='me', connection_name='feed',
-                 message='Muito lega5luj!', place='298042960354301')
+
 
 # Checkin produto
 # graph.put_object(parent_object='me', connection_name='feed',
@@ -18,21 +17,30 @@ graph.put_object(parent_object='me', connection_name='feed',
 
 
 #graph.put_object(parent_object='me', connection_name='/137522203306294/ratings',
-#                 message='Humberto acaba de fazer uma compra na @AppleUS', place='298042960354301')
+#                 message='Humberto acaba de fazer uma compra na @AppleUS', place='
 
 
-
-
-ser = serial.Serial(port='/dev/ttyACM2', baudrate=115200)
+ser = serial.Serial(port='/dev/ttyACM0', baudrate=115200)
 
 def recieving():
     while True:
         retorno = ser.readline()
+        # Esse da pra postar 
 
-        if retorno == 'a\r\n':
+        if retorno == b'checkin\r\n':
+            graph.put_object(parent_object='me', connection_name='feed', message='Postado via Raspberry Pi usando Python'  + str(randint(1, 5000)) , place='298042960354301')
+
             print('entrou')
+            print(retorno)
+            
+        elif retorno == b'share\r\n':
+            graph.put_object(parent_object='me', connection_name='feed', message='Humberto acaba de fazer uma compra na @AppleUS', place='298042960354301')
+
+
+            print('entrou1')
+            print(retorno)
         
-        print(retorno)
+
 
         
         # ser.write(b'')
